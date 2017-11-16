60842023ff1636e9b22fc6340493c26c1253b676
Yassin Siouda <yassin@siouda.com>
2017-11-16 22:31:36 +0100

    feat: adds uikit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>Last fm a year ago</title>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.4/css/components/accordion.almost-flat.css">
         <script src="jquery.js"></script>
     </head>
     <body>
