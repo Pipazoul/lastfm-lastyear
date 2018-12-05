05457d4cbafa3b2cd8eb3653b629a2d0abb4c7cc
ysiouda <ysiouda@erasme.org>
2018-12-05 13:49:46 +0100

    FEAT minir text update

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
                 <div class="uk-card-body ">
                     <form action="fetch.php" method="post">
                     <input class=" uk-textarea" type="text" name="pseudo" id="pseudo" value="<?php echo $pseudo; ?>">
+                    <p>How many years back ?</p>
                     <select name="years" id="years">
                         <option value="1" >1</option>
                         <option value="2" >2</option>
@@ -31,6 +32,7 @@
                         <option value="4" >4</option>
                         <option value="5" >5</option>
                     </select>
+                    year
                     
                     <button id="submit" class="uk-button uk-button-default uk-align-center">Let's go !</button>
                 </form>
